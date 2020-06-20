f788169fb2bba509256d9f6311b3e5ceb1b36c7e
Himalaya vats <hvats.hv@gmail.com>
2020-06-20 14:59:58 +0530

    Winston database connection string was set to localhost in production environment, i set it to environment variables

--- startup/logging.js
+++ startup/logging.js
@@ -2,9 +2,12 @@ require("express-async-errors");
 const winston = require("winston");
 require("winston-mongodb");
 
+const config = require("config");
+
+
 module.exports = function(){
     winston.add(new winston.transports.File({filename : "logfile.log"}));
-    winston.add(new winston.transports.MongoDB({db : "mongodb://localhost/vidly"}));
+    winston.add(new winston.transports.MongoDB({db : config.get("database")}));
 
     winston.exceptions.handle(new winston.transports.File({filename : "uncaughtExceptions.log"}));
 
