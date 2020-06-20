8f1b656ebe3f632533b8584f9a23d25427aa3e56
Himalaya vats <hvats.hv@gmail.com>
2020-06-20 14:40:02 +0530

    Added home page on / route for documantation usage

--- startup/routes.js
+++ startup/routes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 
+const home = require("../routes/home");
 const subjects = require("../routes/subjects");
 const attendance = require("../routes/attendance");
 const register = require("../routes/register");
 const auth = require("../routes/auth");
 
+
 module.exports = function(app) {
+    app.set("view engine", "ejs");
     app.use(express.json());
+    app.use('/', home);
     app.use('/api/subjects', subjects);
     app.use('/api/attendance', attendance);
     app.use('/api/register', register);
